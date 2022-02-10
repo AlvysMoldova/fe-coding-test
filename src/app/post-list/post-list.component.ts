20742f4031012a1cd625e78e824a0569325b0963
Vadim Maniuc <v.maniuc@easyworx.nl>
2022-02-10 19:29:54 +0200

    feat: fetch posts

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from './post';
+import { PostListService } from './post-list.service';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
+  providers: [PostListService],
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postListService: PostListService) {}
 
   ngOnInit(): void {
+    this.getPosts();
   }
 
+  getPosts(): void {
+    this.postListService.getPosts()
+        .subscribe(posts => (this.posts = posts));
+  }
 }
